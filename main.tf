3fa32dbe4bc57127fa020211309f0b67c609f190
Peter Ruissen <34573718+pruissen@users.noreply.github.com>
2025-12-03 00:19:41 +0100

    double entries

--- main.tf
+++ main.tf
@@ -1,21 +1,3 @@
-terraform {
-  required_version = ">= 1.0"
-
-  required_providers {
-    helm = {
-      source  = "hashicorp/helm"
-      version = ">= 2.16"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = ">= 2.30"
-    }
-    kubectl = {
-      source  = "gavinbunney/kubectl"
-      version = ">= 1.14.0"
-    }
-  }
-}
 
 # ============================================================================
 # 1. NAMESPACES
